c3f2ed9bc9b299c22d3be8c19fa5246bac406ad0
Kate <you@example.com>
2021-01-13 09:32:58 +0300

    this commit will register models created

--- poetry/admin.py
+++ poetry/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from poetry.models import Love,Spiritual,Anger,Death,Family,Famous,Friendship,Holiday,Life,Nature,Sad,Christian,Coronavirus
+
 # Register your models here.
+
+admin.models.register(Love)
+
+admin.models.register(Spiritual)
+admin.models.register(Anger)
+admin.models.register(Death)
+admin.models.register(Friendship)
+admin.models.register(Famous)
+admin.models.register(Family)
+admin.models.register(Holiday)
+admin.models.register(Life)
+admin.models.register(Nature)
+admin.models.register(Sad)
+admin.models.register(Coronavirus)
+admin.models.register(Christian)
+
