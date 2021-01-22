44908d5b7fb88c3e73a7a367244d7bb06206b102
Kate <you@example.com>
2021-01-22 14:49:06 +0300

    this commit will add urls

--- poetry/urls.py
+++ poetry/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
-from poetry.views import lovePoems
+from poetry.views import lovePoems,angerPoems,christianPoems,coronavirusPoems,deathPoems,familyPoems,friendshipPoems,holidayPoems,lifePoems,naturePoems,sadPoems,spiritualPoems
 
 urlpatterns=[
     path('', views.index, name='index'),
