f30de7fde6fa072bec517fc7e0ff86e35bd15b33
Kate <you@example.com>
2021-01-22 14:47:13 +0300

    this commit will add views

--- poetry/views.py
+++ poetry/views.py
@@ -26,3 +26,134 @@ def lovePoems(request):
     }
     return render(request,'poetry/lovePoems.html', context)
 
+@login_required(login_url='/login/')
+def angerPoems(request):
+    
+    angerPoems=Anger.objects.all()
+    angerPoems=angerPoems[::-1]
+
+    context={  
+        'angerPoems':angerPoems, 
+    }
+    return render(request,'poetry/angerPoems.html', context)
+
+
+@login_required(login_url='/login/')
+def christianPoems(request):
+    
+    christianPoems=Christian.objects.all()
+    christianPoems=christianPoems[::-1]
+
+    context={  
+        'christianPoems':christianPoems, 
+    }
+    return render(request,'poetry/christianPoems.html', context)
+
+
+@login_required(login_url='/login/')
+def coronavirusPoems(request):
+    
+    coronavirusPoems=Coronavirus.objects.all()
+    coronavirusPoems=coronavirusPoems[::-1]
+
+    context={  
+        'coronavirusPoems':coronavirusPoems, 
+    }
+    return render(request,'poetry/coronavirus.html', context)
+
+
+@login_required(login_url='/login/')
+def deathPoems(request):
+    
+    deathPoems=Death.objects.all()
+    deathPoems=deathPoems[::-1]
+
+    context={  
+        'deathPoems':deathPoems, 
+    }
+    return render(request,'poetry/deathPoems.html', context)
+
+@login_required(login_url='/login/')
+def familyPoems(request):
+    
+    familyPoems=Family.objects.all()
+    familyPoems=familyPoems[::-1]
+
+    context={  
+        'familyPoems':familyPoems, 
+    }
+    return render(request,'poetry/familyPoems.html', context)
+
+
+@login_required(login_url='/login/')
+def friendshipPoems(request):
+    
+    friendshipPoems=Friendship.objects.all()
+    friendshipPoems=friendshipPoems[::-1]
+
+    context={  
+        'friendshipPoems':friendshipPoems, 
+    }
+    return render(request,'poetry/friendshipPoems.html', context)
+
+
+@login_required(login_url='/login/')
+def holidayPoems(request):
+    
+    holidayPoems=Holiday.objects.all()
+    holidayPoems=holidayPoems[::-1]
+
+    context={  
+        'holidayPoems':holidayPoems, 
+    }
+    return render(request,'poetry/holidayPoems.html', context)
+
+
+@login_required(login_url='/login/')
+def lifePoems(request):
+    
+    lifePoems=Life.objects.all()
+    lifePoems=lifePoems[::-1]
+
+    context={  
+        'lifePoems':lifePoems, 
+    }
+    return render(request,'poetry/lifePoems.html', context)
+
+
+@login_required(login_url='/login/')
+def sadPoems(request):
+    
+    sadPoems=Sad.objects.all()
+    sadPoems=sadPoems[::-1]
+
+    context={  
+        'sadPoems':sadPoems, 
+    }
+    return render(request,'poetry/sadPoems.html', context)
+
+
+@login_required(login_url='/login/')
+def spiritualPoems(request):
+    
+    spiritualPoems=Spiritual.objects.all()
+    spiritualPoems=spiritualPoems[::-1]
+
+    context={  
+        'spiritualPoems':spiritualPoems, 
+    }
+    return render(request,'poetry/spiritual.html', context)
+
+
+@login_required(login_url='/login/')
+def naturePoems(request):
+    
+    naturePoems=Nature.objects.all()
+    naturePoems=naturePoems[::-1]
+
+    context={  
+        'naturePoems':naturePoems, 
+    }
+    return render(request,'poetry/naturePoems.html', context)
+
+
